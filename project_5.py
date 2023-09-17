18d01522fa49e4458255d11f0354ebeb534c1ca9
a1adeen <127492547+a1adeen@users.noreply.github.com>
2023-09-17 11:49:36 +0530

    will continue this tommorow

--- project_5.py
+++ project_5.py
@@ -1,4 +1,17 @@
 # face recognition
+import face_recognition
+import cv2
+import numpy as np
+from datetime import datetime
+# import csv
 
 
-hey = 8 
+
+
+video_capture = cv2.VideoCaptureAPIs(0)
+print(dir(cv2))
+
+# load only known imags
+
+user_image_1 = face_recognition.load_image_file("self.jpg")
+user_1_encoding = face_recognition.face_encodings(user_image_1)[0]
